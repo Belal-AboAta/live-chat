1e751a5c6829ac70e93235b454ce5bbe544ece5d
Belal Abo Ata <belalbelalmohamed1234@gmail.com>
2025-10-15 10:38:28 +0300

    feat: scroll to the last message

--- client/src/components/ChatWindow/index.tsx
+++ client/src/components/ChatWindow/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect, useRef } from "react";
 
 import { ChatHeader } from "../ChatHeader";
 import { ChatMessage } from "../ChatMessage";
@@ -22,10 +23,22 @@ export const ChatWindow: React.FC<ChatWindowProps> = ({
   onAttachFile,
   onVioceInput,
 }) => {
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop =
+        messagesContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
     <div className="w-4/5 flex flex-col h-full bg-white rounded-lg shadow-md">
       <ChatHeader title={chatTitle} subtitle={chatSubtitle} />
-      <div className="flex-1 p-4 overflow-y-auto bg-gray-200">
+      <div
+        className="flex-1 p-4 overflow-y-auto bg-gray-200"
+        ref={messagesContainerRef}
+      >
         {/* TODO: add empty messages view */}
         {messages.map((message) => (
           <ChatMessage
